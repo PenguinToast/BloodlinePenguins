ed692bea3b752b7237dc2fa984dda994e1eadf37
William Sheu <java.user.00001@gmail.com>
2013-11-04 22:42:34 -0800

    Resources

--- BloodlinePenguins-desktop/src/com/penguintoast/bloodline/Main.java
+++ BloodlinePenguins-desktop/src/com/penguintoast/bloodline/Main.java
@@ -2,14 +2,17 @@ package com.penguintoast.bloodline;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 
 public class Main {
 	public static void main(String[] args) {
+		TexturePacker2.process("../pack", "../BloodlinePenguins-android/assets/", "game");
+		
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "BloodlinePenguins";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 600;
+		cfg.height = 600;
 		
 		new LwjglApplication(new BloodlinePenguinsMain(), cfg);
 	}
