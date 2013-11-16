35eb6113fb2e41613c5bf0bbe0faf52259fdcb5f
William Sheu <java.user.00001@gmail.com>
2013-11-16 12:12:12 -0800

    WIP Client Join - Fixed classpath

--- BloodlinePenguins/src/com/penguintoast/bloodline/net/GameClient.java
+++ BloodlinePenguins/src/com/penguintoast/bloodline/net/GameClient.java
@@ -15,19 +15,19 @@ import com.penguintoast.bloodline.net.objects.PlayerLeft;
 
 public class GameClient {
 	private LobbyScreen lobby;
-	
+
 	private Client client;
 	private Semaphore lock;
 	private Object received;
-	
+
 	public void setLobby(LobbyScreen screen) {
 		lobby = screen;
 	}
-	
+
 	public void start() {
 		try {
 			lock = new Semaphore(0);
-			
+
 			client = new Client();
 
 			Network.register(client);
@@ -39,47 +39,51 @@ public class GameClient {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	public boolean joinServer(InetAddress addr) {
 		try {
-			client.connect(2000, addr, Network.TCP_PORT, Network.UDP_PORT);
-			client.sendTCP(PlayerData.getInstance());
+			synchronized (client) {
+				client.connect(2000, addr, Network.TCP_PORT, Network.UDP_PORT);
+				client.sendTCP(PlayerData.getInstance());
+			}
 			return true;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 	}
-	
+
 	public InfoResponse queryInfo(InetAddress addr) {
 		try {
-			if(client.isConnected()) {
-				return null;
-			}
-			client.connect(2000, addr, Network.TCP_PORT, Network.UDP_PORT);
-			client.sendTCP(new InfoRequest());
-			lock.acquire();
-			client.close();
-			if(received instanceof InfoResponse) {
-				return (InfoResponse) received;
+			synchronized (client) {
+				if (client.isConnected()) {
+					return null;
+				}
+				client.connect(2000, addr, Network.TCP_PORT, Network.UDP_PORT);
+				client.sendTCP(new InfoRequest());
+				lock.acquire();
+				client.close();
+				if (received instanceof InfoResponse) {
+					return (InfoResponse) received;
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	public void received(Object o) {
 		received = o;
 		lock.release();
-		if(o instanceof PlayerData) {
+		if (o instanceof PlayerData) {
 			PlayerData.updateInstance((PlayerData) o);
 		}
-		if(o instanceof PlayerJoined) {
+		if (o instanceof PlayerJoined) {
 			PlayerData dat = ((PlayerJoined) o).player;
 			lobby.playerJoined(dat);
 			Network.players.put(dat.id, dat);
 		}
-		if(o instanceof PlayerLeft) {
+		if (o instanceof PlayerLeft) {
 			int id = ((PlayerLeft) o).id;
 			lobby.playerLeft(id);
 			Network.players.remove(id);
